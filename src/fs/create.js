463829286fdf361a4886f57dd6162a58ae94d125
Irina <irenserge@mail.ru>
2023-06-17 23:01:21 +0300

    feat: rename.js done

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,11 +2,11 @@ import { writeFile } from 'fs/promises';
 import { existSync } from 'node:fs';
 
 const STRING = 'Here soe text';
-const PATH = './src/fs/files/fresh.txt'
+const PATH = './src/fs/files/fresh.txt';
 
 const create = async () => {
     if (existSync(PATH)) {
-        throw Error('Ooops, operation failed!!!')
+        throw Error('Ooops, operation failed!!!');
     } else {
         try {
             await writeFile(PATH, STRING, {});
