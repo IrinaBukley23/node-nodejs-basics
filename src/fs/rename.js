d7d541a9ab4d7163cb52766b882ba8320715b21a
Irina <irenserge@mail.ru>
2023-06-17 23:11:26 +0300

    feat: delete.js done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs/promises';
 
-const prev = new URL("./files/wrongFilename.txt", import.meta.url);
+// const prev = new URL("./files/wrongFilename.txt", import.meta.url);
 const next = new URL("./files/properFilename.md", import.meta.url);
 
 const rename = async () => {
