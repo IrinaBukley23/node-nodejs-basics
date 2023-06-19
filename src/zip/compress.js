2626e6a4967ae5387d15f9a4063b70c48c0fcbc9
Irina <irenserge@mail.ru>
2023-06-19 21:52:54 +0300

    feat: compress.js done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+const PATH = new URL("./files/fileToCompress.txt", import.meta.url);
+const RES_PATH = new URL("./files/archive.gz", import.meta.url);
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        const gzip = createGzip();
+        const source = createReadStream(PATH);
+        const destination = createWriteStream(RES_PATH);
+        await pipeline(source, gzip, destination);
+      } catch (error) {
+        throw Error('Ooops, operation failed!!!');
+      }
 };
 
 await compress();
