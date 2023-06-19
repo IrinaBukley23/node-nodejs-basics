6476a0db34d5004ca7c57dffedd64fcf56f15555
Irina <irenserge@mail.ru>
2023-06-19 17:55:16 +0300

    feat: env, args & calcHash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { readFile } from "node:fs/promises";
+const { createHash } = await import("node:crypto");
+import path from "path";
+const filePath = new URL("./files/fileToCalculateHashFor.txt", import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash("sha256");
+
+    try {
+        const contents = await readFile(filePath);
+        await hash.update(contents);
+        console.log(`${`${hash.digest("hex")} ${path.basename(filePath.pathname)}`}`);
+    } catch (err) {
+        throw Error('Ooops, operation failed!!!');
+    }
 };
 
 await calculateHash();
