5a7d8a41138e5048f270ccba75f63e8dcc03e365
Irina <irenserge@mail.ru>
2023-06-19 22:11:51 +0300

    feat: worker.js done

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,19 @@
-// n should be received from main thread
+import { parentPort } from "worker_threads";
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    parentPort.on("message", (num) => {
+        const n = parseInt(num, 10);
+        if (!isNaN(n)) {
+          parentPort.postMessage({ status: "resolved", data: nthFibonacci(n) });
+        } else {
+          parentPort.postMessage({ status: "error", data: null });
+        }
+      });
+      parentPort.on("error", () => {
+        parentPort.postMessage({ status: "error", data: null });
+      });
 };
 
 sendResult();
