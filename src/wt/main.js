de9d94e3e2696212c2825122a3f055bee117bedf
Irina <irenserge@mail.ru>
2023-06-19 22:13:40 +0300

    feat: main.js done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,20 @@
+import { parentPort, Worker } from "worker_threads";
+import os from "node:os";
+const PATH = new URL("./worker", import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpus = os.cpus();
+    const promises = cpus.map((info, i) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(PATH);
+      worker.postMessage(10 + i);
+      worker.on("message", (msg) => {
+        resolve(msg);
+      });
+    });
+  });
+  console.log(await Promise.allSettled(promises));
+  process.exit();
 };
 
 await performCalculations();
